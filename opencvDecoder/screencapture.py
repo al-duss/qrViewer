e435f2c72a0817508241b98452f910c13e0cf9d3
al-duss <alex93udssault@gmail.com>
2016-12-04 15:49:06 -0500

    Added salt file, remove pictures smetimes

--- opencvDecoder/screencapture.py
+++ opencvDecoder/screencapture.py
@@ -4,11 +4,13 @@ import sys
 import datetime
 import qrdecode.qrdecode
 import time
+import os
 
 def take_screenshot(counter, frame, minute):
     file_name = str(counter) + "qr.jpg"
     cv2.imwrite(file_name, frame)
     qrdecode.qrdecode.decode_qr(file_name, minute)
+    os.remove(file_name)
 
 cap = cv2.VideoCapture(0)
 current_minute = 0
@@ -18,20 +20,20 @@ while(True):
     now = datetime.datetime.now()
     # Our operations on the frame come here
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
+    
     # Display the resulting frame
     cv2.imshow('frame',gray)
 
-    if current_minute != now.minute:
-        current_minute = now.minute
-        img_prefix = int(time.time() * 1000)
-        take_screenshot(img_prefix, frame, now.minute)
-
-    #or smaller time? like 10 seconds?
-    # if current_minute != now.second and now.second % 10 == 0:
-    #     current_minute = now.second
+    # if current_minute != now.minute:
+    #     current_minute = now.minute
     #     img_prefix = int(time.time() * 1000)
     #     take_screenshot(img_prefix, frame, now.minute)
+
+    #or smaller time? like 10 seconds?
+    if current_minute != now.second and now.second % 10 == 0:
+        current_minute = now.second
+        img_prefix = int(time.time() * 1000)
+        take_screenshot(img_prefix, frame, now.minute)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         print "Exiting..."
